82e4aa9e8093eb65de07c8522afba18f6d226bbc
matheusdcunha <matheus.cunha.fonseca50@gmail.com>
2025-03-23 23:56:07 -0300

    feat: Add SignUp HTTP Request

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,23 @@
 import { useState } from "react"
+import { useNavigate } from "react-router";
+
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+
+import { api } from "../services/api"
+
+const signUpSchema = z.object({
+  name: z.string().trim().min(1, { message: "Informe o nome." }),
+  email: z.string().email({ message: "Email inválido." }),
+  password: z.string().min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+  passwordConfirm: z.string({ message: "Confirme a senha" })
+}).refine((data) => data.password === data.passwordConfirm, {
+  message: "As senhas não são iguais",
+  path: ["passwordConfirm"]
+})
 
 export function SignUp() {
   const [name, setName] = useState("")
@@ -9,10 +26,42 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate()
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault()
 
-    alert(`${name},${email}, ${password} ${passwordConfirm}`)
+    try {
+      setIsLoading(true)
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm
+      })
+
+      await api.post("/users", data)
+
+      if (confirm("Cadastrado com Sucesso. Ir para tela de entrar?")) {
+        navigate("/")
+      }
+
+    } catch (error) {
+      console.log(error)
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message)
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+
+      alert("Não foi possível cadastrar")
+    } finally {
+      setIsLoading(false)
+    }
+
   }
 
 
@@ -52,7 +101,7 @@ export function SignUp() {
       </Button>
 
       <a href="/" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">
-      Já tenho uma conta
+        Já tenho uma conta
       </a>
 
     </form>
